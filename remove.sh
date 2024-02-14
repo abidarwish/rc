3133b3bdf0aec316cbe2004cd1e3c976ef372fbd
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2024-02-14 14:08:41 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -2,10 +2,10 @@
 #script by Abi Darwish
 
 if [ -n $(pgrep -f change_ip); then
-kill -9 $(pgrep -f change_ip)
+	kill -9 $(pgrep -f change_ip)
 fi
 if [ -n $(pgrep -f /etc/arca/change_ip) ]; then
-kill -9 $(pgrep -f /etc/arca/change_ip)
+	kill -9 $(pgrep -f /etc/arca/change_ip)
 fi
 
 sed -i '/^.*pgrep -f change_ip/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
