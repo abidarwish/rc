82fb7bb00556eb78a7dfdfaef9565e0f7e8e36bc
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2024-02-14 14:07:34 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 #script by Abi Darwish
 
+if [ -n $(pgrep -f change_ip); then
+kill -9 $(pgrep -f change_ip)
+fi
+if [ -n $(pgrep -f /etc/arca/change_ip) ]; then
 kill -9 $(pgrep -f /etc/arca/change_ip)
+fi
 
-sed -i '/^.*pgrep -f restart_wan/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
-sed -i '/^.*pgrep -f \/etc\/arca\/restart_wan/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
-sed -i '/if \[ -e \/etc\/arca\/restart_wan \].*$/,/fi/d' /usr/lib/rooter/connect/create_connect.sh
+sed -i '/^.*pgrep -f change_ip/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
+sed -i '/^.*pgrep -f \/etc\/arca\/change_ip/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
+sed -i '/if \[ -e \/etc\/arca\/change_ip \].*$/,/fi/d' /usr/lib/rooter/connect/create_connect.sh
 
 rm -rf /etc/arca/change_ip
