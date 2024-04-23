eae29a67897708081a8528a0179caea7819476eb
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2024-04-24 06:22:06 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -11,9 +11,11 @@ fi
 sed -i '/^.*pgrep -f change_ip/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
 sed -i '/^.*pgrep -f \/etc\/arca\/change_ip/d;/^$/d' /usr/lib/rooter/connect/create_connect.sh
 sed -i '/if \[ -e \/etc\/arca\/change_ip \].*$/,/fi/d' /usr/lib/rooter/connect/create_connect.sh
+sed -i '/\/etc\/arca\/counter/d' /etc/init.d/rooter
 
 if [ -e /usr/lib/rooter/connect/conmon.sh.bak ]; then
         mv /usr/lib/rooter/connect/conmon.sh.bak /usr/lib/rooter/connect/conmon.sh
+	chmod 755 /usr/lib/rooter/connect/conmon.sh
 fi
 
 rm -rf /etc/arca/change_ip
