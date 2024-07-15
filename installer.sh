099189dff2145a98512f1330c0546abf286f7b5f
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2024-07-15 08:24:01 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -112,10 +112,11 @@ while true; do
                         if [ $(grep -c -o R13 /etc/arca/quectel_version) -ne 0 ]; then
                                 MBIMChangeWANIP
                                 sleep 15
+                                log "MBIM Protocol restarted"
                         else
                                 /usr/lib/rooter/gcom/gcom-locked /dev/ttyUSB2 run-at.gcom 1 "AT+CFUN=1,1"
-                        fi        
-                        log "MBIM Protocol restarted"
+                                log "Modem module restarted"
+                        fi
                 else
                         log "Modem protocol not detected"
                         /usr/lib/rooter/gcom/gcom-locked /dev/ttyUSB2 run-at.gcom 1 "AT+CFUN=1,1"
